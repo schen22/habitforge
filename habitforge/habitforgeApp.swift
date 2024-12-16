283d87ad431241622b3b1654c668ea6a4dbb5479
schen22 <sarahchen22@gmail.com>
2024-12-15 17:53:32 -0800

    retry

---
+++ habitforge/habitforgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitforgeApp.swift
+//  habitforge
+//
+//  Created by Sarah Chen on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct habitforgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
